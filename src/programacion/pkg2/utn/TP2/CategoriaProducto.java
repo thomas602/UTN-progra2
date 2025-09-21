ca98b8ac9962153a58e71a446fa0a49488c54096
thomas602 <48530650+thomas602@users.noreply.github.com>
2025-09-21 10:53:46 -0300

    Add CategoriaProducto enum for product categories

---
+++ src/programacion/pkg2/utn/TP2/CategoriaProducto.java
@@ -0,0 +1,18 @@
+package programacion.pkg2.utn;
+
+public enum CategoriaProducto {
+    ALIMENTOS("Productos comestibles"),
+    ELECTRONICA("Dispositivos electrónicos"),
+    ROPA("Prendas de vestir"),
+    HOGAR("Artículos para el hogar");
+
+    private final String descripcion;
+
+    CategoriaProducto(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+}
